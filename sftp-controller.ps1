15e03b34f8780fc138aaa682ab1131d20d795a8d
Vladyslav Hnatovskyi <34673396+vhnatovskyi@users.noreply.github.com>
2024-09-03 10:59:35 -0600

    Update sftp-controller.ps1

--- sftp-controller.ps1
+++ sftp-controller.ps1
@@ -7,5 +7,20 @@ param(
 # Import the Az.Storage module
 Import-Module Az.Storage
 
-# Set the SFTP setting on the storage account
-Set-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -EnableSftp $EnableSftp
+# Увійти в Azure
+Connect-AzAccount -Identity
+
+# Вимкнути SFTP для облікового запису зберігання
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
+
+if ($storageAccount) {
+    $storageAccount.SkuName = "Standard_LRS"  # Або інший тип, який вам потрібен
+    $storageAccount.EnableSftp = $false
+
+    # Оновлення налаштувань облікового запису
+    Set-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -EnableSftp $EnableSftp
+    
+    Write-Output "SFTP successfully updated for $StorageAccountName to $EnableSftp."
+} else {
+    Write-Output "Not Found $StorageAccountName."
+}
