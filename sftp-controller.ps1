17c4febc2d96a0c842139db3419ac8b63cfa8aaa
Vladyslav Hnatovskyi <34673396+vhnatovskyi@users.noreply.github.com>
2024-09-03 13:04:13 -0600

    Update sftp-controller.ps1

--- sftp-controller.ps1
+++ sftp-controller.ps1
@@ -4,19 +4,13 @@ param(
     [bool]$EnableSftp
 )
 
-# Import the Az.Storage module
 Import-Module Az.Storage
-
-# Увійти в Azure
 Connect-AzAccount -Identity
-
-# Вимкнути SFTP для облікового запису зберігання
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
 
 if ($storageAccount) {
     $storageAccount.EnableSftp = $false
 
-    # Оновлення налаштувань облікового запису
     Set-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -EnableSftp $EnableSftp -DisableLocalUsers $DisableLocalUsers
     
     Write-Output "SFTP successfully updated for $StorageAccountName to $EnableSftp."
