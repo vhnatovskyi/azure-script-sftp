29af0adee0c91d7395ea31e1e60b6fd69a8889d3
Vladyslav Hnatovskyi <34673396+vhnatovskyi@users.noreply.github.com>
2024-09-03 09:50:54 -0600

    Update sftp-controller.ps1

--- sftp-controller.ps1
+++ sftp-controller.ps1
@@ -7,13 +7,4 @@ param(
 # Import the Az.Storage module
 Import-Module Az.Storage
 
-# Get the storage account
-$storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
-
-# Update the SFTP settings
-$storageAccount.UpdateProperties.sftpEnabled = $EnableSftp
-
-# Apply the update
-$storageAccount | Set-AzStorageAccount
-
-#Update-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -EnableSftp $EnableSftp
+Update-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -EnableSftp $EnableSftp
